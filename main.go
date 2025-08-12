675b21d01fe5c7bd8c496dff34bf5ba6d206de4a
altr-r <arafatrahman0012@gmail.com>
2025-08-12 17:59:11 +0600

    switch case

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	learningmaps "hello_world/learning_maps"
 	learningstructs "hello_world/learning_structs"
+	switchcase "hello_world/switch_case"
 )
 
 func main() {
 	learningmaps.LearningMaps()
 	learningstructs.LearningStructs()
+	switchcase.SwitchCaseExample()
 }
